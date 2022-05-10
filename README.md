81e74cfc35b019fbf664491e65011d74cdbbaedc
Sanket <code.sanket13@gmail.com>
2022-05-10 16:35:59 +0530

    Basic App

---
+++ README.md
@@ -0,0 +1,7 @@
+# Follow the steps -
+
+- git clone https://github.com/sanketghosh/alpine-todo.git
+- cd
+- npm install
+- npm run build
+- npm run watch
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+
+        darkBlue: "hsl(228, 39%, 23%)",
+        darkGrayishBlue: "hsl(227, 12%, 61%)",
+        veryDarkBlue: "hsl(233, 12%, 13%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+      },
+    },
+  },
+  plugins: [],
+};
